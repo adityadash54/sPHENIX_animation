da51151f7f650dae0e8aaddbc4926402cad5d73e
Aditya Prasad Dash <aditya@Adityas-MacBook-Air-2.local>
2023-03-21 17:40:56 -0700

    Working version of TPC drift animation with user defined drift speed(along all axes) and user defined number of tracks

--- Animatingtracks.py
+++ Animatingtracks.py
@@ -7,14 +7,15 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 import array
 
-f=open('TRACKS2.json') #else use the folder option on the left column and upload the file manually
+f=open('TRACKS2_21March.json') #else use the folder option on the left column and upload the file manually
 data_json=json.load(f)
 print(data_json)
 f.close()
 
 #User defined values
-drift_speed=[0.0,0.0,1.0]
-no_tracks=5 #number of tracks to animate it will start from track 1
+drift_speed_posz=np.array([0.0,0.0,1.0])
+drift_speed_negz=np.array([0.0,0.0,-1.0])
+no_tracks=10 #number of tracks to animate it will start from track 1
 
 
 
@@ -30,25 +31,24 @@ print("innertracker_arr=",innertracker_arr)
 
 #Getting the cluster positions of a track
 x_y_z_clusters=[]
-lentrack1=0
 for track_no in range(no_tracks):
     x_y_z_dict_track=innertracker_arr[track_no] #innertracker_arr[i] reads the ith track
     x_y_z_clusters_track=np.array(x_y_z_dict_track['pts']) #2D array the x,y,z positions corresponding to each cluster e.g. x_y_z[0][0] is the x coordinate of the 1st cluster
     #for jj in range(len(x_y_z_clusters_track)):
         #x_y_z_clusters.append(x_y_z_clusters_track[jj])
-    print("x_y_z_clusters_track[0]=")
-    print(x_y_z_clusters_track[0])
+    #print("x_y_z_clusters_track[0]=")
+    #print(x_y_z_clusters_track[0])
     
     if(track_no==0):
         x_y_z_clusters=x_y_z_clusters_track
-        print("track_no==0, x_y_z_clusters[0]=")
-        print(x_y_z_clusters[0])
-        lentrack1=len(x_y_z_clusters_track)
+        #print("track_no==0, x_y_z_clusters[0]=")
+        #print(x_y_z_clusters[0])
+        #lentrack1=len(x_y_z_clusters_track)
     
     else:
         x_y_z_clusters=np.append(x_y_z_clusters,x_y_z_clusters_track,axis=0)
-        print("track_no!=0, x_y_z_clusters[lentrack1]=")
-        print(x_y_z_clusters[lentrack1])
+        #print("track_no!=0, x_y_z_clusters["+str(lentrack1)+"]=")
+        #print(x_y_z_clusters[lentrack1])
 
     print("shape of x_y_z clusters=")
     print(x_y_z_clusters.shape)
@@ -66,19 +66,39 @@ for track_no in range(no_tracks):
 #ANIMATION
 #based on https://medium.com/@pnpsegonne/animating-a-3d-scatterplot-with-matplotlib-ca4b676d4b55
 
-start_speed = np.zeros(x_y_z_clusters.shape)
+start_speed_posz = np.zeros(x_y_z_clusters.shape)
+start_speed_negz = np.zeros(x_y_z_clusters.shape)
 
 
-for jj in range(len(start_speed)):
-  start_speed[jj]=drift_speed
+for jj in range(len(start_speed_posz)):
+  start_speed_posz[jj]=drift_speed_posz
+  start_speed_negz[jj]=drift_speed_negz
 
+#We can do this faster by separating the clusters with z>0 and z<0 and rearranging the cluster with z>0 on top of z<0 then we do not need to check if z>0 for every iteration
 #print(start_speed)
 data = [x_y_z_clusters]
-nbr_iterations=10
+#print(data)
+        
+nbr_iterations=100
 for iteration in range(nbr_iterations):
-        previous_positions = data[-1]
-        new_positions = previous_positions + start_speed
+        previous_positions = np.copy(data[-1]) #otherwise the values in data[-1] change when we change values in previous_positions be careful
+        new_positions=np.copy(previous_positions) #initialisation
+        
+        for jj in range(len(previous_positions)):
+            if(previous_positions[jj][2]>0):
+                new_positions[jj] = previous_positions[jj] + drift_speed_posz
+            elif(previous_positions[jj][2]<0):
+                #print(new_positions[jj])
+                new_positions[jj] = previous_positions[jj] + drift_speed_negz
+                #print("negz")
+                #print(new_positions[jj])
+            else:
+                print("Track found with z postion exactly equal to 0, it stays there")
+        #print("new positions")
+        #print(new_positions)
         data.append(new_positions)
+        #print(data)
+
         #if(iteration<5):
         #  print("previous_positions=")
         #  print(previous_positions)
@@ -86,7 +106,6 @@ for iteration in range(nbr_iterations):
         #  print(new_positions)
 
         
-#data
 
 def animate_scatters(iteration, data, scatters):
 #    """
