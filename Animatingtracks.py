c1f2d1400510db666cfab2511ac8a342cda5b6bd
Aditya Prasad Dash <aditya@Adityas-MacBook-Air-2.local>
2023-03-22 18:12:21 -0700

    Added TPC surface and speed information to Animatingtracks.py

--- Animatingtracks.py
+++ Animatingtracks.py
@@ -9,13 +9,13 @@ import array
 
 f=open('TRACKS2_21March.json') #else use the folder option on the left column and upload the file manually
 data_json=json.load(f)
-print(data_json)
+#print(data_json)
 f.close()
 
 #User defined values
-drift_speed_posz=np.array([0.0,0.0,1.0])
-drift_speed_negz=np.array([0.0,0.0,-1.0])
-no_tracks=20 #number of tracks to animate it will start from track 1
+drift_speed_posz=np.array([0.0,0.0,0.8]) #z distance travelled in cm per iteration(20ms as fps is 50) in the animation #Actual drift speed=8cm/microsecond, so here it is scaled by 5*10^-6 i.e. in animation speed is 0.04cm/millisecond
+drift_speed_negz=np.array([0.0,0.0,-0.8])
+no_tracks=2 #number of tracks to animate it will start from track 1
 
 
 
@@ -24,15 +24,15 @@ numpy_array_json = np.array(list(dict_json))
 mom_dict=numpy_array_json[3][1] #only works for this format the 3rd row is TRACKS and and 1 refers to INNERTRACKER
 innertracker_arr=mom_dict['INNERTRACKER'] #stores the tracks information as an array
 
-print("Input Dictionary =",dict_json)
-print("The resultant numpy array:\n", numpy_array_json)
-print("mom_dict=",mom_dict)
-print("innertracker_arr=",innertracker_arr)
+#print("Input Dictionary =",dict_json)
+#print("The resultant numpy array:\n", numpy_array_json)
+#print("mom_dict=",mom_dict)
+#print("innertracker_arr=",innertracker_arr)
 
 #Getting the cluster positions of a track
 x_y_z_clusters=[]
 for track_no in range(no_tracks):
-    x_y_z_dict_track=innertracker_arr[track_no] #innertracker_arr[i] reads the ith track
+    x_y_z_dict_track=innertracker_arr[track_no].copy() #innertracker_arr[i] reads the ith track
     x_y_z_clusters_track=np.array(x_y_z_dict_track['pts']) #2D array the x,y,z positions corresponding to each cluster e.g. x_y_z[0][0] is the x coordinate of the 1st cluster
     #for jj in range(len(x_y_z_clusters_track)):
         #x_y_z_clusters.append(x_y_z_clusters_track[jj])
@@ -40,7 +40,7 @@ for track_no in range(no_tracks):
     #print(x_y_z_clusters_track[0])
     
     if(track_no==0):
-        x_y_z_clusters=x_y_z_clusters_track
+        x_y_z_clusters=np.copy(x_y_z_clusters_track)
         #print("track_no==0, x_y_z_clusters[0]=")
         #print(x_y_z_clusters[0])
         #lentrack1=len(x_y_z_clusters_track)
@@ -65,23 +65,14 @@ for track_no in range(no_tracks):
 
 #ANIMATION
 #based on https://medium.com/@pnpsegonne/animating-a-3d-scatterplot-with-matplotlib-ca4b676d4b55
-
-start_speed_posz = np.zeros(x_y_z_clusters.shape)
-start_speed_negz = np.zeros(x_y_z_clusters.shape)
-
-
-for jj in range(len(start_speed_posz)):
-  start_speed_posz[jj]=drift_speed_posz
-  start_speed_negz[jj]=drift_speed_negz
-
 #We can do this faster by separating the clusters with z>0 and z<0 and rearranging the cluster with z>0 on top of z<0 then we do not need to check if z>0 for every iteration
-#print(start_speed)
+
 data = [x_y_z_clusters]
 #print(data)
         
 nbr_iterations=100
 for iteration in range(nbr_iterations):
-        previous_positions = np.copy(data[-1]) #otherwise the values in data[-1] change when we change values in previous_positions be careful
+        previous_positions = np.copy(data[-1]) #use np.copy() otherwise the values in data[-1] change when we change values in previous_positions
         new_positions=np.copy(previous_positions) #initialisation
         
         for jj in range(len(previous_positions)):
@@ -127,6 +118,19 @@ def animate_scatters(iteration, data, scatters):
         
     return scatters
 
+def TPC_surface(center_x,center_y,inner_radius,outer_radius, height_z): #https://stackoverflow.com/questions/26989131/add-cylinder-to-plot
+    ngridpoints=30
+    z = np.linspace(-height_z, height_z, ngridpoints)
+    theta = np.linspace(0, 2*np.pi, ngridpoints)
+    theta_grid, z_grid=np.meshgrid(theta, z)
+    x_grid_inner = inner_radius*np.cos(theta_grid) + center_x
+    y_grid_inner = inner_radius*np.sin(theta_grid) + center_y
+    x_grid_outer = outer_radius*np.cos(theta_grid) + center_x
+    y_grid_outer = outer_radius*np.sin(theta_grid) + center_y
+    
+    return x_grid_inner,y_grid_inner,x_grid_outer,y_grid_outer,z_grid
+
+
 def main(data, save=False):
 #    """
 #    Creates the 3D figure and animates it with the input data.
@@ -138,6 +142,11 @@ def main(data, save=False):
     # Attaching 3D axis to the figure
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    #Drawing TPC
+    Xc_in,Yc_in,Xc_out,Yc_out,Zc = TPC_surface(0.0,0.0,20,80,105)
+    ax.plot_surface(Xc_in, Yc_in, Zc, alpha=0.3)
+    ax.plot_surface(Xc_out, Yc_out, Zc, alpha=0.3)
+    
     #ax = p3.Axes3D(fig)
 
     # Initialize scatters
@@ -156,16 +165,16 @@ def main(data, save=False):
     ax.set_zlim3d([-100, 100])
     ax.set_zlabel('Z')
 
-    ax.set_title('Track drifting in TPC')
+    ax.set_title('Clusters drifting in TPC (speed scaled by 5*10^(-6)')
 
     # Provide starting angle for the view.
     ax.view_init(25, 90,0,'y')
 
     ani = animation.FuncAnimation(fig, animate_scatters, iterations, fargs=(data, scatters),
-                                       interval=1000/iterations, blit=False, repeat=True)
+                                       interval=20, blit=False, repeat=True) #interval is in milliseconds and is the time between each frame
 
     if save:
-        ani.save('Animated_tracks.mp4',writer='ffmpeg',fps=iterations)
+        ani.save('Animated_tracks.mp4',writer='ffmpeg',fps=50)
         #ani.save('Animated_tracks.gif',writer='Pillow',fps=iterations)
         
         #Writer = animation.writers['ffmpeg']
