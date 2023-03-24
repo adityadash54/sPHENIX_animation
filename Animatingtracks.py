d2c105b9ddbc9af8c4f47aeb6e194c5ebcab841c
Aditya Prasad Dash <aditya@Adityas-MacBook-Air-2.local>
2023-03-24 14:59:36 -0700

    Line 91 Appending only values of new_positions which are less than 105, line 119 setting scatters[i] to (0,0,-150) if the point goes beyond TPC

--- Animatingtracks.py
+++ Animatingtracks.py
@@ -15,7 +15,7 @@ f.close()
 #User defined values
 drift_speed_posz=np.array([0.0,0.0,1.6]) #z distance travelled in cm per iteration(20ms as fps is 50) in the animation #Actual drift speed=8cm/microsecond, so here it is scaled by 10^-5 i.e. in animation speed is 0.08cm/millisecond
 drift_speed_negz=np.array([0.0,0.0,-1.6])
-no_tracks=20 #number of tracks to animate it will start from track 1
+no_tracks=2 #number of tracks to animate it will start from track 1
 
 
 
@@ -70,16 +70,16 @@ for track_no in range(no_tracks):
 data = [x_y_z_clusters]
 #print(data)
         
-nbr_iterations=100
+nbr_iterations=1000
 for iteration in range(nbr_iterations):
         previous_positions = np.copy(data[-1]) #use np.copy() otherwise the values in data[-1] change when we change values in previous_positions
-        previous_positions=previous_positions[abs(previous_positions[:,2])<105]
+        #previous_positions=previous_positions[abs(previous_positions[:,2])<105]
         new_positions=np.copy(previous_positions) #initialisation
         
         for jj in range(len(previous_positions)):
-            if(previous_positions[jj][2]>0 and previous_positions[jj][2]<105):
+            if(previous_positions[jj][2]>0):
                 new_positions[jj] = previous_positions[jj] + drift_speed_posz
-            elif(previous_positions[jj][2]<0 and previous_positions[jj][2]>-105):
+            elif(previous_positions[jj][2]<0):
                 #print(new_positions[jj])
                 new_positions[jj] = previous_positions[jj] + drift_speed_negz
                 #print("negz")
@@ -88,8 +88,11 @@ for iteration in range(nbr_iterations):
                 #print("Track found with z postion exactly equal to 0 or reached endcap, it stays there")
         #print("new positions")
         #print(new_positions)
+        new_positions=new_positions[abs(new_positions[:,2])<105] #retaining only the clusters inside TPC
+        if(len(new_positions)==0):
+            break
         data.append(new_positions)
-        #print(data)
+        print(data)
 
         #if(iteration<5):
         #  print("previous_positions=")
@@ -109,8 +112,11 @@ def animate_scatters(iteration, data, scatters):
 #    Returns:
 #        list: List of scatters (One per element) with new coordinates
 #    """
-    for i in range(data[iteration].shape[0]):
-        scatters[i]._offsets3d = (data[iteration][i,0:1], data[iteration][i,1:2], data[iteration][i,2:])
+    for i in range(data[0].shape[0]):
+        if(i<data[iteration].shape[0]):
+            scatters[i]._offsets3d = (data[iteration][i,0:1], data[iteration][i,1:2], data[iteration][i,2:])
+        else:
+            scatters[i]._offsets3d = ([0], [0], [-150])
         #if(iteration==1):
          #   if(i==1):
          #       print(data[iteration][i,0:1])
@@ -157,13 +163,13 @@ def main(data, save=False):
     iterations = len(data)
 
     # Setting the axes properties
-    ax.set_xlim3d([-100, 100])
+    ax.set_xlim3d([-200, 200])
     ax.set_xlabel('X')
 
-    ax.set_ylim3d([-100, 100])
+    ax.set_ylim3d([-200, 200])
     ax.set_ylabel('Y')
 
-    ax.set_zlim3d([-120, 120])
+    ax.set_zlim3d([-200, 200])
     ax.set_zlabel('Z')
 
     ax.set_title('Clusters drifting in TPC (speed scaled by 5*10^(-6))')
